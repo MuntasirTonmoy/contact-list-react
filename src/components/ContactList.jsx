4b021903648965eea858ad81a4aeb7ead599ee1f
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2023-02-01 23:48:50 +0600

    key added to card component

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -3,7 +3,7 @@ import ContactCard from "./ContactCard";
 
 const ContactList = props => {
   const renderList = props?.contacts.map(contact => {
-    return <ContactCard contact={contact} />;
+    return <ContactCard contact={contact} key={contact.id} />;
   });
   return (
     <div>
