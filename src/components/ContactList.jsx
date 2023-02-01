6d42a916321e8a2f20c34a86ccb1ebf763b15c19
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2023-02-01 23:27:43 +0600

    contact list added

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -2,7 +2,13 @@ import React from "react";
 
 const ContactList = props => {
   const renderList = props?.contacts.map(contact => {
-    return <div className="">{contact.name}</div>;
+    return (
+      <div className="" key={contact.id}>
+        <h2>{contact.name}</h2>
+        <p>{contact.phone}</p>
+        <p>{contact.email}</p>
+      </div>
+    );
   });
   return <div>{renderList}</div>;
 };
