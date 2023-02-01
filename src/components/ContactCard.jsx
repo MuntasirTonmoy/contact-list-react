849e265e37b1efdbd061997f59afb9e25c6b3347
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2023-02-01 23:48:20 +0600

    contact card component created

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
-const ContactCard = () => {
-  return <div>Contact Card</div>;
+const ContactCard = props => {
+  return (
+    <div className="border-b pb-2 border-b-slate-500">
+      <h2 className="text-xl capitalize mb-1 mt-2">{props.contact.name}</h2>
+      <p>
+        <a
+          href={`tel:${props.contact.phone}`}
+          className="pointer text-slate-900"
+        >
+          {props.contact.phone}
+        </a>
+      </p>
+      <p>
+        <a
+          href={`mailto:${props.contact.email}`}
+          className="pointer text-slate-900"
+        >
+          {props.contact.email}
+        </a>
+      </p>
+    </div>
+  );
 };
 
 export default ContactCard;
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,25 +1,9 @@
 import React from "react";
+import ContactCard from "./ContactCard";
 
 const ContactList = props => {
   const renderList = props?.contacts.map(contact => {
-    return (
-      <div className="border-b pb-2 border-b-slate-500" key={contact.id}>
-        <h2 className="text-xl capitalize mb-1 mt-2">{contact.name}</h2>
-        <p>
-          <a href={`tel:${contact.phone}`} className="pointer text-slate-900">
-            {contact.phone}
-          </a>
-        </p>
-        <p>
-          <a
-            href={`mailto:${contact.email}`}
-            className="pointer text-slate-900"
-          >
-            {contact.email}
-          </a>
-        </p>
-      </div>
-    );
+    return <ContactCard contact={contact} />;
   });
   return (
     <div>
