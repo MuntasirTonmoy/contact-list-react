70dc70f68f7f94cae07102bb581f840d9889aa8c
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2023-02-01 23:07:48 +0600

    add contact styled

--- src/components/AddContact.jsx
+++ src/components/AddContact.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 
 const AddContact = () => {
-  return <div>Add contact</div>;
+  return (
+    <div className=" border border-slate-800 w-[90%] mx-auto my-4 rounded">
+      <h2 className="text-2xl text-center text-slate-200 p-3 bg-slate-500">
+        Add Contact
+      </h2>
+      <form className="p-3">
+        <label htmlFor="name">Name</label>
+        <input
+          id="name"
+          type="text"
+          className="block w-full p-1 my-1.5 focus:border focus:border-slate-400 rounded"
+        />
+        <label htmlFor="phone">Phone</label>
+        <input
+          id="phone"
+          type="tel"
+          className="block w-full p-1 my-1.5 focus:border focus:border-slate-400 rounded"
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          type="tel"
+          className="block w-full p-1 my-1.5 focus:border focus:border-slate-400 rounded"
+        />
+        <button className="p-2 mt-3  w-full bg-blue-500 font-semibold rounded text-slate-200">
+          Add
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default AddContact;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <>
-      <h1 className="text-3xl font-medium text-center py-5 bg-slate-400">
+      <h1 className="text-3xl font-medium text-center py-5 bg-slate-500 text-slate-200">
         Contact List
       </h1>
     </>
