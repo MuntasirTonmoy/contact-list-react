097537ab45d7570c89dcfbda758ea9c74b8d6252
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2023-02-02 22:09:15 +0600

    useeffect added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AddContact from "./components/AddContact";
 import ContactList from "./components/ContactList";
 import Header from "./components/Header";
 
 function App() {
   const [contacts, setContacts] = useState([]);
+
   const addContactHandler = newContact => {
     setContacts([...contacts, newContact]);
   };
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
     <div className="container mx-auto bg-slate-300 min-h-screen">
       <Header />
