fa60fc6ac2749d109de8ac7903806e476ebbfaba
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2023-02-04 00:22:28 +0600

    router setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
-import AddContact from "./components/AddContact";
-import ContactList from "./components/ContactList";
-import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
-  // if any contacts array in local storage then contacts value will be it else an empty array
-  const [contacts, setContacts] = useState(
-    JSON.parse(localStorage.getItem("contacts")) ?? []
-  );
-
-  const addContactHandler = newContact => {
-    setContacts([...contacts, newContact]);
-  };
-
-  const removeContactHandler = id => {
-    const remainingContacts = contacts?.filter(contact => contact.id !== id);
-    setContacts(remainingContacts);
-  };
-
-  useEffect(() => {
-    localStorage.setItem("contacts", JSON.stringify(contacts));
-  }, [contacts]);
-
   return (
-    <div className="container mx-auto bg-slate-300 min-h-screen">
-      <Header />
-      <Routes></Routes>
-      <section className="w-[90%] mx-auto">
-        <AddContact addContactHandler={addContactHandler} />
-        <ContactList contacts={contacts} getId={removeContactHandler} />
-      </section>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
+import Header from "./Header";
 
 const Home = () => {
-  return <div>Home</div>;
+  // if any contacts array in local storage then contacts value will be it else an empty array
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem("contacts")) ?? []
+  );
+
+  const addContactHandler = newContact => {
+    setContacts([...contacts, newContact]);
+  };
+
+  const removeContactHandler = id => {
+    const remainingContacts = contacts?.filter(contact => contact.id !== id);
+    setContacts(remainingContacts);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+  return (
+    <div className="container mx-auto bg-slate-300 min-h-screen">
+      <Header />
+      <section className="w-[90%] mx-auto">
+        <AddContact addContactHandler={addContactHandler} />
+        <ContactList contacts={contacts} getId={removeContactHandler} />
+      </section>
+    </div>
+  );
 };
 
 export default Home;
