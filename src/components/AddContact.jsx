18613eff6f02d215ee1edaa641891bedea895e77
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2023-02-04 22:47:50 +0600

    back button added

--- src/components/AddContact.jsx
+++ src/components/AddContact.jsx
@@ -25,37 +25,42 @@ const AddContact = props => {
     navigate("/");
   };
   return (
-    <div className="my-8 mx-4 border border-slate-800  rounded">
-      <h2 className="text-2xl text-center text-slate-200 p-3 bg-slate-500">
-        Add Contact
-      </h2>
-      <form className="p-3" onSubmit={createContact}>
-        <label htmlFor="name">Name</label>
-        <input
-          id="name"
-          name="name"
-          type="text"
-          className="block w-full p-1 my-1.5 focus:border focus:border-slate-400 rounded"
-        />
-        <label htmlFor="phone">Phone</label>
-        <input
-          id="phone"
-          name="phone"
-          type="tel"
-          className="block w-full p-1 my-1.5 focus:border focus:border-slate-400 rounded"
-        />
-        <label htmlFor="email">Email</label>
-        <input
-          id="email"
-          name="email"
-          type="email"
-          className="block w-full p-1 my-1.5 focus:border focus:border-slate-400 rounded"
-        />
-        <button className="p-2 mt-3  w-full bg-blue-500 font-semibold rounded text-slate-200">
-          Add
-        </button>
-      </form>
-    </div>
+    <>
+      <button className="p-2  mb-4  bg-red-300 w-full  font-semibold">
+        Back
+      </button>
+      <div className="mb-8 mx-4 border border-slate-800  rounded">
+        <h2 className="text-2xl text-center text-slate-200 p-3 bg-slate-500">
+          Add Contact
+        </h2>
+        <form className="p-3" onSubmit={createContact}>
+          <label htmlFor="name">Name</label>
+          <input
+            id="name"
+            name="name"
+            type="text"
+            className="block w-full p-1 my-1.5 focus:border focus:border-slate-400 rounded"
+          />
+          <label htmlFor="phone">Phone</label>
+          <input
+            id="phone"
+            name="phone"
+            type="tel"
+            className="block w-full p-1 my-1.5 focus:border focus:border-slate-400 rounded"
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            className="block w-full p-1 my-1.5 focus:border focus:border-slate-400 rounded"
+          />
+          <button className="p-2 mt-3  w-full bg-blue-500 font-semibold rounded text-slate-200">
+            Add
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
 
